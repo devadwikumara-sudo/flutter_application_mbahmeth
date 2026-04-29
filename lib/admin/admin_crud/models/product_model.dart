7dde10f2dc9ce99900b2fa2ce532cf07fec349e5
alannnn32 <ahmadmaulana32321@gmail.com>
2026-04-29 10:57:30 +0700

    Create Product model with JSON serialization
    Added a Product class with JSON serialization methods.

--- lib/admin/admin_crud/models/product_model.dart
+++ lib/admin/admin_crud/models/product_model.dart
@@ -1,1 +1,42 @@
+class Product {
+  final String? id;
+  final String name;
+  final int price;
+  final int stock;
+  final String category;
+  final String description;
+  final String? imageUrl;
 
+  Product({
+    this.id,
+    required this.name,
+    required this.price,
+    required this.stock,
+    required this.category,
+    required this.description,
+    this.imageUrl,
+  });
+
+  // Untuk mengubah JSON dari PHP ke Object Flutter
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      name: json['nama_produk'],
+      price: int.parse(json['harga']),
+      stock: int.parse(json['stok']),
+      category: json['kategori'],
+      description: json['deskripsi'],
+      imageUrl: json['foto'],
+    );
+  }
+
+  // Untuk mengirim data dari Flutter ke PHP
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "nama_produk": name,
+    "harga": price.toString(),
+    "stok": stock.toString(),
+    "kategori": category,
+    "deskripsi": description,
+  };
+} 
