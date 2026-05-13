74afb1181e6a92585b8d7d0456f09f6f857e4a0a
alannnn32 <ahmadmaulana32321@gmail.com>
2026-05-13 09:34:30 +0700

    Add idCategory field and update JSON mappings

--- lib/models/modelsadmin/product_model.dart
+++ lib/models/modelsadmin/product_model.dart
@@ -1,5 +1,6 @@
 class ProductModel {
   final String? id;
+  final String? idCategory;
   final String name;
   final int price;
   final int stock;
@@ -9,6 +10,7 @@ class ProductModel {
 
   ProductModel({
     this.id,
+    this.idCategory,
     required this.name,
     required this.price,
     required this.stock,
@@ -17,33 +19,38 @@ class ProductModel {
     this.category,
   });
 
-  // localhost ketika upload gambar
-  static const String imageBaseUrl = "http://localhost/api_pertanian/uploads/";
+  // Base URL untuk menampilkan gambar di aplikasi
+  static const String imageBaseUrl =
+      "http://localhost/TOKO_MBAHMETH/public/assets/products/";
 
-  // Getter untuk memanggil URL gambar secara otomatis
+  // Getter untuk memanggil URL gambar lengkap
   String get fullImageUrl => "$imageBaseUrl$imagePath";
 
   factory ProductModel.fromJson(Map<String, dynamic> json) {
     return ProductModel(
-      id: json['id_produk']?.toString(),
+      
+      id: json['id_product']?.toString(),
+      idCategory: json['id_category']?.toString(),
       name: json['nama_produk'] ?? '',
       price: int.tryParse(json['harga'].toString()) ?? 0,
       stock: int.tryParse(json['stok'].toString()) ?? 0,
-      imagePath: json['foto'] ?? '',
+      imagePath:
+          json['gambar_produk'] ?? '',
       description: json['deskripsi'],
-      category: json['kategori'],
+      category:
+          json['nama_kategori'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'id_produk': id,
+      'id_product': id,
+      'id_category': idCategory,
       'nama_produk': name,
       'harga': price,
       'stok': stock,
-      'foto': imagePath,
+      'gambar_produk': imagePath,
       'deskripsi': description,
-      'kategori': category,
     };
   }
 }
